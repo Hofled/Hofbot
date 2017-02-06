cd0d1cfd732e8d3b089dc9ea8e5f7b87bc91e2bf
Hofled <ofri.herold@gmail.com>
2017-02-06 23:10:52 +0200

    removed details from main.ts

--- main/main.ts
+++ main/main.ts
@@ -2,7 +2,7 @@ import { BotConnection } from './connection/index';
 import { TmiOptions } from './definitions/connection-options'
 
 // Authentication key required according to the account which you use for the bot
-let options = new TmiOptions(true, true, 'alkcoins', 'oauth:9u1qyef5r4pttgrtyjhtgr32d1scq6', ["hofled"]);
+let options = new TmiOptions(true, true, 'Hofbot', 'oauth:????', ["hofled"]);
 
 let botConnection = new BotConnection(options);
 
