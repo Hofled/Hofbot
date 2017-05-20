c9b24f945da2d21a78a777b1ee42cf6bf925d15a
Hofled <ofri.herold@gmail.com>
2017-05-20 20:44:46 +0300

    Added display-name to UserDataSchema

--- main/definitions/mongoose-schemes/user-data.schema.ts
+++ main/definitions/mongoose-schemes/user-data.schema.ts
@@ -5,4 +5,5 @@ export class UserDataSchema extends Schema {
     gambling: Schema.Types.Date;
     currencies: [CurrencyDataSchema];
     permission: Schema.Types.Number;
+    "display-name": Schema.Types.String;
 }
